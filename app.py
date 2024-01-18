86c08214f83f4371950408b4492a608e7bbc2fba
Daniel Kinahan <danielkinahan@uvica.ca>
2024-01-18 12:01:52 -0500

    Testing for container update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! Welcome to the demo of k8s-hello-world-builder."
 
 
 if __name__ == "__main__":
